90691016927c9920684f8e3807cc4a35297762e8
unknown <Des.McCarter@LPT2177.bjss.co.uk>
2015-05-15 14:05:50 +0100

    middletier entity files

---
+++ middletier/eapi/src/main/java/com/iscope/rest/GetEntity.java
@@ -0,0 +1,40 @@
+package com.iscope.rest;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import com.iscope.mongodb.*;
+
+@Path("getentity")
+public class GetEntity {
+    
+    @GET
+    @Path("/{parameter}")
+    public Response responseMsg( @PathParam("parameter") String parameter,
+            @DefaultValue("Not given") @QueryParam("id") String id) {
+ 
+
+        if( (id!=null) && (id.length()>0) )
+		{
+        	if( (parameter!=null) && (parameter.length()>0) && (parameter.equals("entity")) )
+        	{
+	        	String entityId=id;
+	        	
+	            String output = "Entity ID = " + entityId;
+	
+	        	return Response.status(200).entity(output).build();
+        	}
+        	else
+        	{
+            	return Response.status(400).entity("Parameter not given").build();
+        	}
+		}
+        else
+        {
+        	return Response.status(400).entity("Value required").build();
+        }
+    }
+}
