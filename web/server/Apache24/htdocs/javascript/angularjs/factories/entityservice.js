f3de6fea39af3eda41b5c063de735c7cca94ef37
mccarterd <desmccarter@hotmail.com>
2015-05-17 21:51:34 +0100

    modified for AJAX

--- web/server/Apache24/htdocs/javascript/angularjs/factories/entityservice.js
+++ web/server/Apache24/htdocs/javascript/angularjs/factories/entityservice.js
@@ -8,18 +8,11 @@ app.service("entityservice",function(){
         {
             $.ajax({
                 url: "http://localhost:8080/eapi/getentity/all",
-                dataType:"jsonp",
-                xhrFields: {
-                // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-                // This can be used to set the 'withCredentials' property.
-                // Set the value to 'true' if you'd like to pass cookies to the server.
-                // If this is enabled, your server must respond with the header
-                // 'Access-Control-Allow-Credentials: true'.
-                withCredentials: false
-                },
+                dataType: 'jsonp',
                 success: function(data) {
                     entities=data;
                 }
+                
             });
         }
         
