8d4b33fa815164aec66c6a5e2b104196bebf1e4e
unknown <desmccarter@hotmail.com>
2015-05-19 14:24:50 +0100

    moved to different java package

---
+++ middletier/entitydb/src/main/java/com/iscope/db/Main.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.iscope.db;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+
+/**
+ * @author Des.McCarter
+ *
+ */
+public class Main {
+	
+	/**
+	 * @param args
+	 * @throws UnknownHostException 
+	 */
+	public static void main(String[] args) throws UnknownHostException {
+		//createEntityEntries();
+	}
+	
+	private static void createEntityEntries(){
+		
+		BasicDBObject record = 
+				new BasicDBObject("_id","database").append("name","database")
+				.append("src", "images/icons/database.jpg");		
+
+		try {
+			EntityMongo entityDb = new EntityMongo();
+		
+			entityDb.removeAllEntities();
+			
+			BasicDBList inputsList = new BasicDBList();
+			
+			inputsList.add(new BasicDBObject("username","").append("password", "").append("domain", "DOMAIN"));
+			
+			record.put("inputs", inputsList);
+			
+			entityDb.insertEntity("database",record);
+			
+			entityDb.insertEntity("queue", "images/icons/queue.png");
+	        
+			entityDb.insertEntity("file", "images/icons/fileserver.png");
+	        
+			entityDb.insertEntity("api", "images/icons/api.png");
+	 
+			entityDb.insertEntity("service", "images/icons/service.png");
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
