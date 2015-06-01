190eec8a164d5367b4c89e0c5773f8d5a18196b2
unknown <desmccarter@hotmail.com>
2015-06-01 11:19:58 +0100

    new Mongo DB tsts

---
+++ middletier/entitydb/src/test/java/com/iscope/db/tests/integration/EntityMongoTest.java
@@ -0,0 +1,46 @@
+package com.iscope.db.tests.integration;
+
+import java.net.UnknownHostException;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.iscope.db.EntityMongo;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+public class EntityMongoTest {
+	
+	private static EntityMongo entityMongo = null;
+	
+	@Before 
+	public void setup(){
+		try {
+			
+			if(entityMongo==null)
+			{
+				entityMongo = new EntityMongo();
+			}
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getDatabaseJSON() {
+	
+		String json = entityMongo.getEntityByName("database");
+		
+		String expectedJson =
+				"{ \"_id\" : \"database\" , \"name\" : \"database\" , \"src\" : \"images/icons/database.jpg\" , \"inputs\" : [ { \"username\" : \"\" , \"password\" : \"\" , \"domain\" : \"DOMAIN\"}]}";
+		
+		Assert.assertEquals(expectedJson, json);
+	}
+}
